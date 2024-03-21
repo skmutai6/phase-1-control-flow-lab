65e8ed626a02c52512cf1bb42c55eae71eec3fd9
skmutai6 <skmutai6@gmail.com>
2024-03-21 05:02:59 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,11 +1,61 @@
+
 function scuberGreetingForFeet(){
   // Write your code here!
+  let feet = 199;
+
+  if (feet <= 400)
+  {
+    scuberGreetingForFeet = 'This one is on me!';
+  }
+  else if (feet > 400 && ride <=2000)
+  {
+    scuberGreetingForFeet = 'That will be twenty bucks.';
+  }
+  else if (feet > 2000)
+  {
+    scuberGreetingForFeet = 'I will gladly take your thirty bucks.';
+  }
+  else (feet > 2500)
+  {
+    scuberGreetingForFeet = 'No can do.';
+  }
+
 }
 
 function ternaryCheckCity(){
   // Write your code here!
+  const city = nyc;
+
+  let nyc;
+
+  const ternaryCheckCity = city >= nyc ? "Ok, sounds good." : "No go.";
+
+  ternaryCheckCity;
+
 }
 
 function switchOnCharmFromTip(){
   // Write your code here!
+  const switchOnCharmFromTip = 'generous';
+
+  let message;
+
+  switch (switchOnCharmFromTip) {
+    case 'generous':
+        message = "Thank you so much."
+        break;
+    
+    case 'not generous':
+        message = "Thank you."
+        break;
+    
+    default:
+        console.log("Bye");
+        break;
+
+
+  }
+
 }
+
+
